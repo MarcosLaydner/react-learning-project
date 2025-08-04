42b83532d9db483a96f7f56e2f9dff2ebf5b7e09
Marcos Laydner <marcoslaydner123@gmail.com>
2025-08-04 10:16:45 -0300

    initial commit - 01 do curso

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className='banner'>
+            <img src="/images/banner.png" alt="Banner Principal da página"/>
+        </header>
+    )
+}
+
+export default Banner
---
+++ src/components/Form/index.js
@@ -0,0 +1,18 @@
+import TextField from '../TextField/TextField'
+import './Form.css'
+
+const Form = () => {
+
+    return (
+        <section className='formulario'>
+        <form >
+            <h2>Peencha os Campo para criar o card do colaborador</h2>
+            <TextField label="Nome" placeholder="Digite seu nome"/>
+            <TextField label="Cargo" placeholder="Digite seu cargo"/>
+            <TextField label="Imagem" placeholder="Digite o endereço da imagem"/>
+        </form>
+        </section>
+  )
+}
+
+export default Form
